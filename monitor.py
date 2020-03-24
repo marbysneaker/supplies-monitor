6179a1b6a2e50f7009dcbe443d49d38f0d090963
marby <marbysneaker@gmail.com>
2020-03-24 03:48:09 -0700

    added urls

--- monitor.py
+++ monitor.py
@@ -3,13 +3,7 @@ import randomheaders
 from bs4 import BeautifulSoup as soup
 
 
-
-source =  requests.get('https://www.walmart.com/ip/Angel-Soft-Toilet-Paper-24-Mega-Rolls-96-Regular-Rolls/341013821',headers = randomheaders.LoadHeader())
-
-tp1 = requests.get('https://www.walmart.com/ip/6-Pack-Scott-Rapid-Dissolving-Toilet-Paper-8-Rolls-Bath-Tissue/217279228?selected=true', headers = randomheaders.LoadHeader())
-
-
-tp= ['https://www.walmart.com/ip/6-Pack-Scott-Rapid-Dissolving-Toilet-Paper-8-Rolls-Bath-Tissue/217279228?selected=true','https://www.walmart.com/ip/Angel-Soft-Toilet-Paper-24-Mega-Rolls-96-Regular-Rolls/341013821']
+tp= ['https://www.walmart.com/ip/6-Pack-Scott-Rapid-Dissolving-Toilet-Paper-8-Rolls-Bath-Tissue/217279228?selected=true','https://www.walmart.com/ip/Angel-Soft-Toilet-Paper-24-Mega-Rolls-96-Regular-Rolls/341013821','https://www.walmart.com/ip/3-Pack-Scott-Rapid-Dissolving-Toilet-Paper-8-Rolls-Bath-Tissue/212484728?selected=true','https://www.walmart.com/ip/Huggies-Simply-Clean-Baby-Wipes-Unscented-704-Count/942573335?selected=true']
 # print(source.status_code)
 # page_soup = soup(tp1.text, "html.parser")
 # tp = (page_soup.find_all('div', class_="prod-ProductOffer-oosMsg prod-PaddingTop--xxs"))
@@ -26,6 +20,6 @@ def walmart_monitor (tp):
             if i.text == 'Out of stock':
                 print('out of stock')
             else:
-                return
+                return i
 
 walmart_monitor(tp)
